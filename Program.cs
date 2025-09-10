f240d2b4649d5f8c8a7c935aed589af0e4d84b37
Nur Pelin <demirelpelin065@gmail.com>
2025-09-10 13:55:49 +0200

    si

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pelle2004
 {
@@ -10,7 +6,7 @@ namespace Pelle2004
     {
         private static void Main(string[] args)
         {
-            // Entry point logic goes here
+            Console.WriteLine("Hello!");
         }
     }
 }
