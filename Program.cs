3ffed6794cc00affc8355082557cb99198e363ff
Nur Pelin <demirelpelin065@gmail.com>
2025-09-10 13:22:14 +0200

    lol

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace Pelle2004
 {
     internal class Program
     {
-        static void Main(string[] args)
-            writeline("I'm a cheff!");
+        private static void Main(string[] args)
         {
+            // Entry point logic goes here
         }
     }
 }
