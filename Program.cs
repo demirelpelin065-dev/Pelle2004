2d98a6e06d3ae14e724bfa2dcc14631c699f5b9e
Nur Pelin <demirelpelin065@gmail.com>
2025-09-11 10:27:07 +0200

    bbb

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Pelle2004
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
